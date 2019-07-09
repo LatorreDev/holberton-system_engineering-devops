d09bd824a9be5d1ba9d9c4aca0d3c11a375975fc
David Latorre <david@latorredev.com>
2019-07-08 22:01:46 -0500

    [Feature] add advanced task

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+SENDER =  ARGV[0].scan(/(?<=from:)[^\]]*/).join
+TO =  ARGV[0].scan(/(?<=to:)[^\]]*/).join
+FLAGS =  ARGV[0].scan(/(?<=flags:)[^\]]*/).join
+
+puts (SENDER + "," + TO + "," + FLAGS)
