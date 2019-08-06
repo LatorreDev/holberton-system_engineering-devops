31930dab0503d36f486693b0c542a222105f9869
David Latorre <david@latorredev.com>
2019-08-06 00:48:40 -0500

    [Feature] add ssh_config with puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+# puppet manifest to modify ssh_config
+
+exec { 'modify ssh':
+command  => 'echo "PasswordAuthentication no\nIdentityFile ~/ssh/holberton" >> /etc/ssh/ssh_config',
+provider => shell,
+}
