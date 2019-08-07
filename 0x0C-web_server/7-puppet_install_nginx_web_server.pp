39e6f03ca58f511115244e304358cd36ae1eaa36
David Latorre <david@latorredev.com>
2019-08-07 02:53:36 -0500

    [Feature] add puppet manifesto to install nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+#puppet manifest to install a nginx server
+exec {'Install nginx':
+command  => 'sudo apt update && sudo apt -y install nginx && echo "Holberton School" | sudo tee /usr/share/nginx/html/index.html &&redirect="\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n" && sudo sed -i "37i $redirect" /etc/nginx/sites-enabled/default && sudo service nginx restart',
+provider => shell,
+}
+
